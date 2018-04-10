dce6f2d312c07d9daa8d2730f8794e98b35d652d
Chris Herbert <herbert@bermanco.com>
2018-04-09 20:45:32 -0400

    Custom login logo

--- web/app/themes/michelleharven/setup/Theme/AdminCustomize.php
+++ web/app/themes/michelleharven/setup/Theme/AdminCustomize.php
@@ -6,7 +6,7 @@ class AdminCustomize {
 
 	public function __construct(){
 
-		// add_action('login_head', array($this, 'custom_login_logo');
+		add_action('login_head', array($this, 'custom_login_logo'));
 
 	}
 
@@ -15,10 +15,10 @@ class AdminCustomize {
 	 */
 	public function custom_login_logo() {
 		echo '<style type="text/css">
-		h1 a { background-image: url('.get_bloginfo('template_directory').'/assets/img/logo.svg) !important;
+		h1 a { background-image: url('.get_bloginfo('template_directory').'/assets/img/login-logo.svg) !important;
 		 background-size: 100% !important;
 		 width: 100% !important;
-		 height: 125px !important;
+		 height: 30px !important;
 		 pointer-events: none;
 		}
 		</style>';

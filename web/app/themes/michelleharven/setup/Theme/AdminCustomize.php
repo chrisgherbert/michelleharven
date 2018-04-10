9342dee90efc443a7f44f3b86e501d15622db93a
Chris Herbert <herbert@bermanco.com>
2018-04-09 21:04:52 -0400

    Switched image for login page

--- web/app/themes/michelleharven/setup/Theme/AdminCustomize.php
+++ web/app/themes/michelleharven/setup/Theme/AdminCustomize.php
@@ -15,10 +15,10 @@ class AdminCustomize {
 	 */
 	public function custom_login_logo() {
 		echo '<style type="text/css">
-		h1 a { background-image: url('.get_bloginfo('template_directory').'/assets/img/login-logo.svg) !important;
-		 background-size: 100% !important;
+		h1 a { background-image: url('.get_bloginfo('template_directory').'/assets/img/login-logo-photo.png) !important;
+		 background-size: 50% !important;
 		 width: 100% !important;
-		 height: 30px !important;
+		 height: 170px !important;
 		 pointer-events: none;
 		}
 		</style>';
